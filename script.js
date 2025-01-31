8ae57145ca1f51bc46523203e75678d7fabb7985
MarcselloDev <88592581+MarcselloDev@users.noreply.github.com>
2025-01-31 20:39:46 +0100

    Create stopwatch & save best time

--- script.js
+++ script.js
@@ -17,17 +17,43 @@ document.getElementById('user-input').addEventListener('input', () => {
   
 })
 
+let seconds
+let stopwatch
+let bestTime = localStorage.getItem('bestTime')
 function startTimer () {
   console.log('Timer Started');
-
+  seconds = 0;
+  stopwatch = setInterval(() => {
+    document.querySelector('#timer').innerHTML = (seconds/100).toFixed(2)
+    seconds++;
+  }, 10);
 }
 
 function stopTimer () {
   console.log('Timer Ended');
   document.getElementById('user-input').value = ''
-}
+  if (seconds < bestTime || bestTime === null) {
+    clearInterval(stopwatch)
+    bestTime = seconds - 1
+    localStorage.setItem('bestTime', bestTime)
+    document.querySelector('#best-time').innerHTML = `${(localStorage.getItem('bestTime')/100).toFixed(2)} seconds`
+  } else{
+    clearInterval(stopwatch)
+  }
 
-function restartTimer () {
-  console.log('Timer Restarted');
   isTyping = false
 }
+
+function restartTimer() {
+  console.log('Timer Restarted');
+  isTyping = false
+  clearInterval(stopwatch)
+  document.querySelector('#timer').innerHTML = (0).toFixed(2)
+  document.querySelector('#user-input').value = ''
+  }
+
+document.querySelector('#start-btn').addEventListener('click', () => {
+  restartTimer()
+})
+
+document.querySelector('#best-time').innerHTML = `${(localStorage.getItem('bestTime')/100).toFixed(2)} seconds`
